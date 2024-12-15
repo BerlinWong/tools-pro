8d6beff9f7ac14d9b2a592439d764dfeb0b04b3c
王柏林 <wzysws@gmail.com>
2024-12-16 04:47:24 +0800

    Updated Navbar and HomePage components with new styles and functionality

--- src/app/pages/XiaoHongShuPage/page.tsx
+++ src/app/pages/XiaoHongShuPage/page.tsx
@@ -51,9 +51,9 @@ const XiaoHongShuPage = () => {
       fetch(`/api/XiaoHongShuPage?xhs_url=${xhsUrl}`, {
         method: "GET",
         headers: {
-          "Content-Type": "application/json",
+          "Content-Type": "text/plain",
           "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0",
           "Referer": "https://www.xiaohongshu.com",
         },
       })
